c5ea09792488729b5bac09a98d523702b3ed09fd
IgorEverton <rm95214@fiap.com.br>
2022-09-14 22:31:04 -0300

    Criado componentes Tarefa e ListaTarefas

---
+++ rm95214/react-aula07/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import ListaTarefas from './components/ListaTarefas'
+
+
+export default function App(){
+    return(
+        <div>
+            <h1>ESTUDO SOBRE STYLED-COMPONENT</h1>
+            <ListaTarefas/>
+           
+        </div>
+    )
+}
---
+++ rm95214/react-aula07/src/components/ListaTarefas.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Tarefa from "./Tarefa";
+
+
+export default function App(){
+    return(
+        <div>
+             
+                <p>Lista de Tarefas</p>
+                <Tarefa/>
+        </div>
+    )
+}

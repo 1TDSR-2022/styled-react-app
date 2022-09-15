1403d485502dba8bd83ebdbf5feed42a77f7836f
BrunaBGM <bruna.gabriela.bgm@gmail.com>
2022-09-14 22:54:38 -0300

    estudo sobre styled componets

---
+++ rm96283/react-aula7/src/components/Tarefa.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const DivTarefa = styled.div`
+  background-color: #bff;
+  border: solid 1px #333;
+    box-shadow: 5px 5px 5px #333;
+    padding: 20px;
+    text-align: center;
+    width: 350px;
+    height: 200px;
+    margin: 10px;
+    h2,
+    p {
+      padding-bottom: 10px;
+    }
+`;
+
+export default function Tarefa() {
+  return (
+    <DivTarefa>
+        <h2>Relatório de vendas</h2>
+        <p>Para:Departamento de vendas</p>
+       <p>Levantar os valores das vendas deste mes</p>
+        </DivTarefa>
+  )
+}

837065d7a13b6972b20cc2166f220b62ada20579
abner-ferreira <rm93576@fiap.com.br>
2022-09-16 21:32:58 -0300

    Mocagem criada no ListaTarefas.

--- rm93576/react-aula7/src/components/ListaTarefas.jsx
+++ rm93576/react-aula7/src/components/ListaTarefas.jsx
@@ -1,15 +1,30 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Tarefa from "./Tarefa.jsx"
 import { DivLista } from '../style/styled.js'
 
 export default function ListaTarefas() {
   
+  const [tarefa,setTarefa] = useState([
+    {
+      "titulo" : "Lista de Pagamento", 
+      "setor" : "Dep. de Vendas", 
+      "descricao" : "Levantar os valores de vendas " 
+    }
+    ,
+    {
+      "titulo" : "Planilha de Salários", 
+      "setor" : "Dep. Pessoal", 
+      "descricao" : "Gerar Planilhas " 
+    }
+
+  ])
+
   return (
     <DivLista>
         <p>Lista de Tarefas</p>
-        <Tarefa/>
-        <Tarefa/>
-        <Tarefa/>
+        <Tarefa
+        
+        />
     </DivLista>
   )
 }
--- rm93576/react-aula7/src/components/Tarefa.jsx
+++ rm93576/react-aula7/src/components/Tarefa.jsx
@@ -1,23 +1,7 @@
 import React from 'react'
-import styled from "styled-components"
+import { DivTarefa } from '../style/styled'
 
-//CRIANDO MÓDULO
-const DivTarefa = styled.div`
-    background-color: #ffb;
-    border: solid 1px #333;
-    box-shadow: 5px 5px 5px #333;
-    padding: 20px;
-    text-align: center;
-    width: 350px;
-    height: 200px;
-    margin: 10px;
-    h2,
-    p {
-      padding-bottom: 10px;
-    }
-`;
-
-export default function Tarefa() {
+export default function Tarefa(props) {
     
   return (
     <DivTarefa>

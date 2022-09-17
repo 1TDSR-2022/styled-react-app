7e4d97edcf5d77b95e77d62664288d7579bfc4fc
AraujoAlan <aalan.araujo@hotmail.com>
2022-09-16 22:38:39 -0300

    addTarefa feito

--- rm95355/react-aula7/src/components/ListaTarefas/ListaTarefas.jsx
+++ rm95355/react-aula7/src/components/ListaTarefas/ListaTarefas.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 import Tarefa from '../Tarefas/Tarefa'
 import { DivLista } from '../style/styled'
+import FormTarefa from '../FormTarefa/FormTarefa'
 
 
 export default function ListaTarefas() {
@@ -19,26 +20,44 @@ export default function ListaTarefas() {
       }
     ])
   
+    const [nTarefa, setNTarefa] = useState({"titulo":"","setor":"","descricao":""})
 
-    const addTarefa = () => {
-      
-      //CRIANDO O OBJETO PARA SER ADD NA LISTA
-      const novaTarefa = 
-      {
-        "titulo" : "Planilha de Notas",
-        "setor" : "Dep. Graduação",
-        "descricao":"Lançar Notas"
+    const addTarefa = (e) => {
+      e.preventDefault()
+      //LIMPANDO O STATE NTAREFA
+      setNTarefa({"titulo":"","setor":"","descricao":""})
+      //ADD O OBJ N TAREFA A LISTA DE OBJS
+      setTarefa([...tarefa, nTarefa])
+    }
+
+    const captura = (e)=>{
+      e.preventDefault()
+
+      const{ name , value } = e.target
+
+      if(name === "titulo"){
+        setNTarefa({"titulo":value,"setor":nTarefa.setor,"descricao":nTarefa.descricao})
+      }else if(name === "setor"){
+        setNTarefa({"titulo":nTarefa.titulo,"setor":value,"descricao":nTarefa.descricao})
+      }else if(name === "descricao"){
+        setNTarefa({"titulo":nTarefa.titulo,"setor":nTarefa.setor,"descricao":value})
       }
 
-      //ADD O OBJ AO STATE
-      setTarefa([...tarefa, novaTarefa])
+
     }
   
     return (
       <DivLista>
 
+      {/* FORM DE CAP DE DADOS DO USUARIO */}
+      <FormTarefa
+        addTarefa={addTarefa}
+        tarefa={nTarefa}
+        digit={captura}
+      />
+
       {/* CRIANDO UM DISPARADOR DA FUNC addTarefa */}
-      <button onClick={addTarefa}>Adicionar</button>
+      {/* <button onClick={addTarefa}>Adicionar</button> */}
 
         {tarefa.map((tar,i)=>
           <Tarefa

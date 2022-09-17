abd72924490e9e053713ba9df71c75c6770b9c08
AraujoAlan <aalan.araujo@hotmail.com>
2022-09-16 21:32:55 -0300

    moc criada na ListaTarefas

--- rm95355/react-aula7/src/App.jsx
+++ rm95355/react-aula7/src/App.jsx
@@ -5,7 +5,7 @@ export default function App() {
   return (
 
     <>
-        <h1>VALENTINA OTARIA</h1>
+        <h1>TAREFAS</h1>
         <ListaTarefas/>
     </>
 
--- rm95355/react-aula7/src/components/ListaTarefas/ListaTarefas.jsx
+++ rm95355/react-aula7/src/components/ListaTarefas/ListaTarefas.jsx
@@ -1,15 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Tarefa from '../Tarefas/Tarefa'
 import { DivLista } from '../style/styled'
 
+
 export default function ListaTarefas() {
+
+  const [tarefa, setTarefa] = useState([
+    {
+      "Título" : "Lista de Pagamento",
+      "Setor" : "Dep. de Vendas",
+      "Descricao" : "Levantar os valores de vendas"
+    },
+    {
+      "Título" : "Planilha de Salários",
+      "Setor" : "Dep. Pessoal",
+      "Descricao" : "Gerar Planilhas"
+    }
+  ])
 
   return (
 
     <DivLista>
-        <Tarefa/>
-        <Tarefa/>
-        <Tarefa/>
+        <Tarefa
+        
+        />
     </DivLista>
   )
 }

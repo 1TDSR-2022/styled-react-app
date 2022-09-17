863f2065fa7bf5557d7080fd760eb320044a4ac8
alecarlosjesus <profalexandre.jesus@fiap.com.br>
2022-09-16 22:37:27 -0300

    AddTarefa concluído.

--- pf0670/react-aula7/src/components/Listatarefas.jsx
+++ pf0670/react-aula7/src/components/Listatarefas.jsx
@@ -1,6 +1,7 @@
 import React,{ useState } from 'react'
 import Tarefa from './Tarefa'
 import { DivLista } from '../style/styled'
+import FormTarefas from './FormTarefas'
 
 
 export default function ListaTarefas() {
@@ -19,24 +20,45 @@ export default function ListaTarefas() {
     }
   ])
 
-    const addTarefa = ()=>{
-      
-      //CRIANDO O OBJETO PARA SER ADICIONADO NA LISTA
-      const novaTarefa = {
-          "titulo" : "Planilha de Notas",
-          "setor" : "Dep. Graduação",
-          "descricao":"Lançar Notas"
+    const [nTarefa, setNTarefa] = useState({"titulo":"","setor":"","descricao":""})
+
+    const addTarefa = (e)=>{
+      e.preventDefault()
+      //LIMPANDO O STATE nTarefa
+      setNTarefa({"titulo":"","setor":"","descricao":""})
+      //ADICIONANDO O OBJETO nTarefa A LISTA  DE OBJETOS
+      setTarefa([...tarefa, nTarefa])
+    }
+
+    const captura = (e)=>{
+      e.preventDefault()
+
+      const{ name , value } = e.target
+
+      if(name === "titulo"){
+        setNTarefa({"titulo":value,"setor":nTarefa.setor,"descricao":nTarefa.descricao})
+      }else if(name === "setor"){
+        setNTarefa({"titulo":nTarefa.titulo,"setor":value,"descricao":nTarefa.descricao})
+      }else if(name === "descricao"){
+        setNTarefa({"titulo":nTarefa.titulo,"setor":nTarefa.setor,"descricao":value})
       }
 
-      //ADICIONANDO O OBJETO AO STATE
-      setTarefa([...tarefa, novaTarefa])
     }
+
 
   return (
     <DivLista>
+      
+    {/* Form de captura de dados do usuário. */}
+    <FormTarefas
+      addTarefa={addTarefa}
+      tarefa={nTarefa}
+      digit={captura}
+    />
+
 
     {/* Criando um disparador da função addTarefa */}
-    <button onClick={addTarefa}>Adicionar</button>
+    {/* <button onClick={addTarefa}>Adicionar</button> */}
 
       {tarefa.map((tar,i)=>
         <Tarefa
